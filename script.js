3fe071ee6d08fcc11c76c14492460b9ac7ec4692
Emmanuel <emmanuelokello294@gmail.com>
2024-03-27 17:23:11 +0300

    updates Ui

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-
-
-function add() {
-    const input = document.querySelector('#newtask input');
-    if (input.value.length === 0) {
-        alert("Kindly Enter Task Name!!!!");
-    } else {
-        const taskName = input.value;
-        const taskId = `taskname-${Date.now()}`; // Generate a unique ID for each task
-        document.querySelector('#tasks').insertAdjacentHTML('beforeend', `
-            <div class="task">
-                <span id="${taskId}">
-                    ${taskName}
-                </span>
-                <button class="delete">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-                </button>
-            </div>
-        `);
-
-        // Event delegation for delete buttons
-        document.querySelector('#tasks').addEventListener('click', function(event) {
-            if (event.target.classList.contains('delete')) {
-                event.target.parentNode.remove();
-            }
-        });
-    }
-}
-
-// Attach the add function to the 'push' button
-document.querySelector('#push').addEventListener('click', add);
+document.getElementById('addTaskButton').addEventListener('click', function() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    const newTask = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            taskList.removeChild(newTask);
+        }
+    });
+    newTask.textContent = taskInput.value;
+    newTask.appendChild(checkbox);
+    taskList.appendChild(newTask);
+    taskInput.value = '';
+});
